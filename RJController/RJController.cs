15d86ba8df4eeb079dc91bae735785b4381e838c
Leszek Kruk <leszek.a.kruk@gmail.com>
2017-03-28 01:29:03 +0200

    add test_filedType class

--- RJController/RJController.cs
+++ RJController/RJController.cs
@@ -8,7 +8,7 @@ namespace RJController
 {
     class RJController
     {
-
+        
     }
 
     public class Person
---
+++ RJDashboard.Tests/Laybel/Test_FieldType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RJDashboard.Tests.Laybel
+{
+    public class Test_FieldType 
+    {
+
+    }
+
+
+}
