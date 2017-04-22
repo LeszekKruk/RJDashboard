10cef456494111f7020c3cb9ad0774491b2c8d6d
Leszek Kruk <leszek.a.kruk@gmail.com>
2017-04-22 22:12:04 +0200

    new tests

---
+++ RJController/Label/LabelLayout.cs
@@ -0,0 +1,116 @@
+﻿using RJPaths;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace RJController.Label
+{
+    public class LabelLayout
+    {
+        private List<IManagementable> _variableObjects;
+
+        public LabelLayout(string jobFile)
+        {
+            _variableObjects = new List<IManagementable>();
+            GetLabelsForJob(jobFile);
+        }
+
+        public List<IManagementable> VariableObjects
+        {
+            get
+            {
+                return _variableObjects;
+            }
+        }
+
+        private void GetLabelsForJob(string jobFile)
+        {
+            try
+            {
+                using (XmlTextReader xmlTextReader = new XmlTextReader(jobFile))
+                {
+                    XPathNodeIterator xpathNodeIterator = new XPathDocument(xmlTextReader, XmlSpace.Preserve).CreateNavigator().Select(XMLNodes.JOB_XML_PATH_Label);
+                    while (xpathNodeIterator.MoveNext())
+                    {
+                        string labelFileName = xpathNodeIterator.Current.GetAttribute("filename", "");
+                        string groupName = xpathNodeIterator.Current.GetAttribute("group", "");
+
+                        try
+                        {
+                            string pathLabelFileName = Path.GetPathRoot(jobFile) + NameFolders.LABELS + labelFileName;
+
+                            GetObjectsFromLabel(groupName, pathLabelFileName);
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                throw new FileNotFoundException("File not found");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void GetObjectsFromLabel(string groupName, string pathLabelFileName)
+        {
+            try
+            {
+                XElement label = XElement.Load(pathLabelFileName);
+                IEnumerable<LabelObject> objects = 
+                              from obj in label.Elements("Label").Elements("Layout").Elements("Object")
+                              select new LabelObject()
+                              {
+                                  ObjectName = (string)obj.Attribute("name"),
+                                  Contents = from con in obj.Elements("Content").Elements()
+                                             where con.Parent.Name == "Content"
+                                             select new ObjectContent()
+                                             {
+                                                 ContentType = con.Name.LocalName,
+                                                 ContentName = (string)con.Attribute("name"),
+                                                 ContentValue = (string)con.Attribute("value")
+                                             }
+                              };
+
+                GetVariableContents(groupName, _variableObjects, objects);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private static void GetVariableContents(string groupName, List<IManagementable> _variableObjects, IEnumerable<LabelObject> objects)
+        {
+            foreach (var obj in objects)
+            {
+                foreach (var content in obj.Contents)
+                {
+                    if (content.ContentType == "Variable")
+                    {
+                        IManagementable pm = new PrintManagement();
+
+                        pm.GroupName = groupName;
+                        pm.ObjectName = obj.ObjectName;
+                        pm.ContentName = content.ContentName;
+                        pm.ContentValue = content.ContentValue;
+                        pm.OutputControl = -1;
+                        pm.DataField = -1;
+
+                        _variableObjects.Add(pm);
+                    }
+                }
+            }
+        } 
+    }
+}
---
+++ RJController/Label/ObjectContent.cs
@@ -0,0 +1,97 @@
+﻿using RJController.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RJController.Label
+{
+    public class ObjectContent : IObjectContent
+    {
+        private string _contentType;
+        private string _contentName;
+        private string _contentValue;
+        int _dataField;
+        int _outputControl;
+
+        public ObjectContent() { }
+
+        public ObjectContent(string contentName, string contentValue, string contentType, int dataField, int outputControl)
+        {
+            if (string.IsNullOrEmpty(contentName))
+            {
+                throw new System.ArgumentException("Parameter can't be null");
+            }
+            else
+            {
+                _contentName = contentName;
+            }
+
+            _contentValue = contentValue;
+            _contentType = contentType;
+            _dataField = dataField;
+            _outputControl = outputControl;
+        }
+
+        public string ContentName
+        {
+            get
+            {
+                return _contentName;
+            }
+            set
+            {
+                _contentName = value;
+            }
+        }
+
+        public string ContentValue
+        {
+            get
+            {
+                return _contentValue;
+            }
+            set
+            {
+                _contentValue = value;
+            }
+        }
+
+        public string ContentType {
+            get {
+                return _contentType;            
+            }
+            set {
+                _contentType = value;
+            }
+        }
+
+        public int OutputControl
+        {
+            get
+            {
+                return _outputControl;
+            }
+
+            set
+            {
+                _outputControl = value;
+            }
+        }
+
+        public int DataField
+        {
+            get
+            {
+                return _dataField;
+            }
+
+            set
+            {
+                _dataField = value;
+            }
+        }
+
+    }
+}
