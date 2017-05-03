b6e08c437dd1a92fa4cfd552491e8146a0cb4216
Leszek Kruk <leszek.a.kruk@gmail.com>
2017-05-03 12:24:38 +0200

    added user control for logging evens, errors and logs

---
+++ RJDashboard/Controls/TextLogs.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace RJDashboard.Controls
+{
+    partial class TextLogs
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLogs = new MetroFramework.Controls.MetroTextBox();
+            this.SuspendLayout();
+            // 
+            // txtLogs
+            // 
+            this.txtLogs.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtLogs.BackColor = System.Drawing.Color.White;
+            this.txtLogs.CustomBackground = true;
+            this.txtLogs.Location = new System.Drawing.Point(15, 15);
+            this.txtLogs.Margin = new System.Windows.Forms.Padding(2);
+            this.txtLogs.Multiline = true;
+            this.txtLogs.Name = "txtLogs";
+            this.txtLogs.ReadOnly = true;
+            this.txtLogs.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLogs.Size = new System.Drawing.Size(362, 358);
+            this.txtLogs.Style = MetroFramework.MetroColorStyle.White;
+            this.txtLogs.TabIndex = 14;
+            this.txtLogs.Text = "Zdarzenia";
+            // 
+            // TextLogs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtLogs);
+            this.Name = "TextLogs";
+            this.Size = new System.Drawing.Size(395, 385);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox txtLogs;
+    }
+}
---
+++ RJDashboard/Controls/TextLogs.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RJLogger;
+
+namespace RJDashboard.Controls
+{
+    public partial class TextLogs : UserControl
+    {
+        private int _count = 0;
+        private List<string> _listLogs;
+
+        public int MaxLogs { get; set; }
+
+        public TextLogs()
+        {
+            MaxLogs = 100;
+            _listLogs = new List<string>();
+
+            InitializeComponent();
+        }        
+
+        public void AddLog(string log, string error, string code)
+        {
+            try
+            {
+                _count += 1;
+
+                txtLogs.AppendText(Environment.NewLine);
+                txtLogs.AppendText($"- {_count} - -- - -- - -- - -- - -- - -- -");
+
+                if (!string.IsNullOrEmpty(log))
+                {                        
+                    txtLogs.AppendText(Environment.NewLine);
+                    txtLogs.AppendText(log);
+
+                    _listLogs.Add(log);
+                }
+
+                if (!string.IsNullOrEmpty(error))
+                {
+                    txtLogs.AppendText(Environment.NewLine);
+                    txtLogs.AppendText($"Error: {error}");
+
+                    _listLogs.Add($"Error: {error}");
+                }
+
+                if (!string.IsNullOrEmpty(code))
+                {
+                    txtLogs.AppendText(Environment.NewLine);
+                    txtLogs.AppendText($"Code: {code}");
+
+                    _listLogs.Add($"Code: {code}");
+                }
+
+                if (_count > MaxLogs)
+                    ClearLogs();
+            }
+            catch (Exception e)
+            {
+                txtLogs.Clear();
+                _count = 0;
+
+                AppLogger.GetLogger().Error("Błąd podczas dodawania loga.", e);
+            }
+        }
+
+        private void ClearLogs()
+        {
+            try
+            {
+                _count = 0;
+                txtLogs.Clear();
+            }
+            catch (Exception e)
+            {
+                AppLogger.GetLogger().Error("Błąd podczas czyszczenia logów", e);
+            }
+        }
+    }
+}
